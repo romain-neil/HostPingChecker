443d7a8f406d3cd9c4776c2a4dcb205b4d6e5994
Romain Neil <tutosfaciles48@gmail.com>
2021-04-27 12:23:36 +0200

    feat(VirtualFileSystemConnector): add connector for virtual filesystem

---
+++ src/main/java/fr/chsn/hostpingchecker/utils/connector/VirtualFileSystemConnector.java
@@ -0,0 +1,20 @@
+package fr.chsn.hostpingchecker.utils.connector;
+
+public class VirtualFileSystemConnector extends AbstractConnector {
+
+	@Override
+	public void write(String path, String content) {
+
+	}
+
+	@Override
+	public String read(String path) {
+		return null;
+	}
+
+	@Override
+	public void close() {
+		//
+	}
+
+}
