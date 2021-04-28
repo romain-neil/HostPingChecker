a5e62bbaf0b1199d3553a52b27d1a8d1b8b67cd0
Romain Neil <tutosfaciles48@gmail.com>
2021-04-28 09:18:04 +0200

    test(VirtualFileSystemConnector): add missing test cases

--- src/test/java/VirtualFileSystemConnectorTest.java
+++ src/test/java/VirtualFileSystemConnectorTest.java
@@ -1,17 +1,34 @@
 import fr.chsn.hostpingchecker.utils.connector.VirtualFileSystemConnector;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class VirtualFileSystemConnectorTest {
 
+	static VirtualFileSystemConnector vFsConnector;
+
+	@BeforeAll
+	static void init() {
+		vFsConnector = new VirtualFileSystemConnector();
+	}
+
 	@Test
 	void testConstructObj() {
-		VirtualFileSystemConnector connector = new VirtualFileSystemConnector();
+		Assertions.assertDoesNotThrow(() -> vFsConnector.read("/dev/null"));
+		assertNull(vFsConnector.read(""));
+	}
 
-		Assertions.assertDoesNotThrow(() -> connector.read("/dev/null"));
-		assertNull(connector.read(""));
+	@Test
+	void testWriteDoesNotThrow() {
+		assertDoesNotThrow(() -> vFsConnector.write("", ""));
+	}
+
+	@Test
+	void testCloseDoesNotThrow() {
+		assertDoesNotThrow(() -> vFsConnector.close());
 	}
 
 }
